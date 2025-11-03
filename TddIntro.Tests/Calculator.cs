07fcf760cd55cd68392d90982fe3a214b72fa83d
zabronax <zabronax@gmail.com>
2025-11-03 11:38:29 +0100

    replace with more real Caculator class specification

---
+++ TddIntro.Tests/Calculator.cs
@@ -0,0 +1,31 @@
+﻿namespace TddIntro.Tests;
+
+public class Calculator_Specification
+{
+    [Fact]
+    public void ThereExistsACalculatorClass()
+    {
+        // Arrange & Act
+        var calculator = new Calculator.Calculator();
+
+        // Assert
+        Assert.NotNull(calculator);
+    }
+
+    [Fact]
+    public void AddMethod_AddNumbers_Correctly()
+    {
+        // Arrange
+        var a = 3;
+        var b = 3;
+        var expected = 6;
+
+        var calculator = new Calculator.Calculator();
+
+        // Act
+        var result = calculator.Add(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
