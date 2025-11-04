3c3768881888b2f8355624f21f2b55c1ac029805
zabronax <zabronax@gmail.com>
2025-11-04 10:49:58 +0100

    fix: implement score keeping for known players

--- PlayerScoreService/PlayerScoreService.cs
+++ PlayerScoreService/PlayerScoreService.cs
@@ -2,13 +2,30 @@
 
 public class PlayerScoreService
 {
+  Dictionary<string, int> _score;
+
+  public PlayerScoreService()
+  {
+    _score = new Dictionary<string, int>();
+  }
+
   public int? GetScore(string playerAlias)
   {
-    return null;
+    var isRegistered = _score.TryGetValue(playerAlias, out int result);
+
+    if (isRegistered)
+    {
+      return result;
+    }
+    else
+    {
+      return null;
+    }
+
   }
 
   public void Add(string playerAlias, int score)
   {
-
+    _score.Add(playerAlias, score);
   }
 }
