a77d121fae915003d18d48aa6ca5e5b6d90de52b
zabronax <zabronax@gmail.com>
2025-11-06 11:20:04 +0100

    setup Api test fixture

--- PlayerScoreApi/Program.cs
+++ PlayerScoreApi/Program.cs
@@ -4,3 +4,8 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+// We need this to use the API in tests
+// It makes this program available for introspection
+// by other projects.
+public partial class Program { }
---
+++ PlayerScoreService.E2E/ApiEndToEndTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace PlayerScoreService.E2E;
+
+// Testing a full Web server requires some setup
+// In essence this imports the whole Web API server
+// and wraps/mounts it into a Test class (Fixture)
+// which allows us to probe and make assertion against
+// the server more easily.
+public class ApiEndToEndTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    // We keep a persistent reference to the client here
+    // meaning that every tests, inside this class
+    // will be using the same Web API instance.
+    private readonly HttpClient _client;
+
+    public ApiEndToEndTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task ScoreEndpointExists()
+    {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
